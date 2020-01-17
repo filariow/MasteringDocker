782fff32d4fdce73b25a52e37d7b5e7b4107ffa4
Alessandro Lo Manto <alessandro.lo.manto@gmail.com>
2020-01-17 19:14:46 +0100

    Vagrant fix ubuntu 16.04

--- tools/Vagrant/provisioning/setup.sh
+++ tools/Vagrant/provisioning/setup.sh
@@ -4,14 +4,6 @@ set -e
 #Update package
 sudo apt-get -y update
 
-# Add locals to avoid "WARNING! Your environment specifies an invalid locale." notifications
-sudo sh -c 'printf "LANGUAGE=\"en_US.UTF-8\"\nLC_ALL=\"en_US.UTF-8\"\n" >> /etc/default/locale'
-export LANGUAGE=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export DEBIAN_FRONTEND=noninteractive
-locale-gen en_US.UTF-8
-sudo dpkg-reconfigure locales
 
 chmod +x /vagrant/provisioning/docker-install.sh /vagrant/provisioning/pull-image.sh /vagrant/provisioning/zsh-install.sh
 
